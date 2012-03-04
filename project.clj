e177d73522bd3f4ba908a3eb4b8a320840d2cead
Masamitsu Oikawa <oicawa@gmail.com>
2012-03-04 22:46:03 +0900

    leiningen project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clominal "1.0.0-SNAPSHOT"
+  :description "Clominal will be a clojure terminal constructed by Swing components."
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :main clominal.core)
