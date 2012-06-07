c33e9bb57104e1041519efb917ef1848c5a6fe1a
Masamitsu Oikawa <oicawa@gmail.com>
2012-06-08 06:27:22 +0900

    It had been changed swing Look & Feel from metal to nimbus.

--- src/clominal/core.clj
+++ src/clominal/core.clj
@@ -1,13 +1,11 @@
+(System/setProperty "awt.useSystemAAFontSettings" "on")
+(javax.swing.UIManager/setLookAndFeel "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel")
+
 (ns clominal.core
   (:require [clominal.action :as action]
             [clominal.keys.keymap :as keymap]
             [clominal.platforms.frame :as frame])
-  (:import (javax.swing UIManager))
   (:gen-class))
 
-;(compile 'clominal.keys.LastKeyAction)
-
 (defn -main [& args]
-  ;(System/setProperty "awt.useSystemAAFontSettings" "on")
-  ;(UIManager/setLookAndFeel "com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel")
   (. frame/*frame* setVisible true))
