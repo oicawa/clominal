efb32c9cd567986ca250a24746ce78abf0ec705e
Masamitsu Oikawa <oicawa@gmail.com>
2012-11-30 02:04:52 +0900

    Forgot adding these files...

---
+++ repl.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+BASE_PATH=${0%/*}
+LIB_PATH=${BASE_PATH}/lib
+SRC_PATH=${BASE_PATH}/src
+CLS_PATH=${BASE_PATH}/classes
+
+java \
+-cp \
+${SRC_PATH}:\
+${CLS_PATH}:\
+${LIB_PATH}/clojure-1.4.0.jar:\
+${LIB_PATH}/clojure-contrib-1.2.0.jar \
+clojure.main -r ${SRC_PATH}/clominal/core.clj
