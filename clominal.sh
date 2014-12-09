66bc3cde3bbd1928620eaa4d982a8df328f0562d
oicawa <oicawa@gmail.com>
2014-12-09 17:25:08 +0900

    Update clominal.sh
    Modified to use leiningen.

--- clominal.sh
+++ clominal.sh
@@ -1,21 +1,4 @@
 #!/bin/sh
 
-BASE_PATH=${0%/*}
-LIB_PATH=${BASE_PATH}/lib
-SRC_PATH=${BASE_PATH}/src
-CLS_PATH=${BASE_PATH}/classes
-#CFG_PATH=${HOME}/.clominal
-CFG_PATH=${BASE_PATH}/plugins
-
-java \
--cp \
-.:\
-${CFG_PATH}:\
-${SRC_PATH}:\
-${CLS_PATH}:\
-${LIB_PATH}/clojure-1.4.0.jar:\
-${LIB_PATH}/clojure-contrib-1.2.0.jar:\
-${LIB_PATH}/rsyntaxtextarea-2.0.6.jar \
-clojure.main -i ${SRC_PATH}/clominal/core.clj
-
+lein run
 echo "Finished..."
